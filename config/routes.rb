9e1c1acf51ea932d552dd170a22741f32d2c1de1
Rafael F. Silveira <rafael.fsra@gmail.com>
2018-04-30 19:34:03 -0300

    Alter endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  mount GraphiQL::Rails::Engine, at: "/gq", graphql_path: "/graphql" if Rails.env.development?
+  post "/gql", to: "graphql#execute"
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/gql" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
